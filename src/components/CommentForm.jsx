fbbeb7e254809f357db48d776ad5ac13d4fda212
Milee_Kayastha <mkayastha@hamropatro.com>
2024-02-25 13:04:18 +0545

    feat: working on commentForm component

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,27 +1,49 @@
 import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 
-const CommentForm = () => {
-  const [text, setText] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+const CommentForm = ({
+  handleSubmit,
+  submitLabel = "Add",
+  hasCancelButton = false,
+  handleCancel,
+  initialText = "",
+}) => {
+  const [text, setText] = useState(initialText);
+  const isTextareaDisabled = text.length === 0;
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!text) return;
+    handleSubmit(text);
     setText("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={onSubmit}>
       <TextField
         variant="outlined"
+        fullWidth
         multiline
-        rows={4}
+        rows={2}
+        sx={{ background: "white" }}
         placeholder="Add your comment..."
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <Button type="submit" variant="contained" color="primary">
-        Add Comment
-      </Button>
+      <div className="comment-form-action">
+        {hasCancelButton && (
+          <Button type="button" variant="outlined" onClick={handleCancel}>
+            Cancel
+          </Button>
+        )}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disabled={isTextareaDisabled}
+        >
+          {submitLabel}
+        </Button>
+      </div>
     </form>
   );
 };
