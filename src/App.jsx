8443ec58bc2ee9554f781855e596151970d36770
Milee_Kayastha <mkayastha@hamropatro.com>
2024-02-25 19:23:03 +0545

    feat: code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 // App.js
 import React from "react";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { store } from "./store";
-import { selectUser } from "./store/userSlice";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import { store } from "./store";
 
 const App = () => {
-  // const dispatch = useDispatch();
-
-  // const handleLogout = () => {
-  //   dispatch(clearUser());
-  // };
 
   return (
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </Provider>
